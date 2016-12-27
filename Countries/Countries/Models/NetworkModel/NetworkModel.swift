c8a457150a4e97023d67434b99cffcfdeebb50de
Vladimir Budniy <vladimir.budniy@Antons-MacBook-Air.local>
2016-12-27 15:27:40 +0200

    project with Landscape method

--- Countries/Countries/Models/NetworkModel/NetworkModel.swift
+++ Countries/Countries/Models/NetworkModel/NetworkModel.swift
@@ -14,15 +14,49 @@ let requestPage = "&page="
 
 class NetworkModel {
     
-    static func load(perPage: String = "30", page: String, block: @escaping (Array<Country>) -> ()) {
-
-        let requestURL = requestedURL + perPage + requestFormat + requestPage + page
+//    static func loadCountries(perPage: String = "30", page: String, block: @escaping (Array<Country>) -> ()) {
+//
+//        let requestURL = requestedURL + perPage + requestFormat + requestPage + page
+//        let url = URL(string: requestURL)
+//        let request = URLRequest(url: url!)
+//        
+//        let config = URLSessionConfiguration.default
+//        let session = URLSession(configuration: config)
+//
+//        let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
+//            if error == nil {
+//                do{
+//                    let json = try JSONSerialization.jsonObject(with: data!, options:.allowFragments)
+//                    let array = ParsingModel.parsJSONCountries(json: json as! Array<Any>)
+//                    
+//                    DispatchQueue.main.async {
+//                        block(array)
+//                    }
+//
+//                }catch {
+//                    print("Error with Json: \(error)")
+//                }
+//            }
+//            
+//            let httpResponse = response as! HTTPURLResponse
+//            let statusCode = httpResponse.statusCode
+//            if statusCode == 200 {
+//                print("Data has been loaded successfully")
+//            }
+//        })
+//        
+//        task.resume()
+//    }
+    
+    static func loadCountries(block: @escaping (Array<Country>) -> ()) {
+        
+        let requestURL = "https://restcountries.eu/rest/v1/all"
         let url = URL(string: requestURL)
         let request = URLRequest(url: url!)
         
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
-
+        
         let task = session.dataTask(with: request, completionHandler: { (data, response, error) in
             if error == nil {
                 do{
@@ -32,7 +66,7 @@ class NetworkModel {
                     DispatchQueue.main.async {
                         block(array)
                     }
-
+                    
                 }catch {
                     print("Error with Json: \(error)")
                 }
--- Countries/Countries/UI/CountriesViewController/CountriesViewController.swift
+++ Countries/Countries/UI/CountriesViewController/CountriesViewController.swift
@@ -14,6 +14,8 @@ class CountriesViewController: UIViewController, ViewControllerRootView, UITable
     
     typealias RootViewType = CountriesView
     
+    var landscapeOrientation: Bool = false
+    
     var refreshControl: UIRefreshControl?
     
     var requestPage = 1
@@ -29,12 +31,25 @@ class CountriesViewController: UIViewController, ViewControllerRootView, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
         self.addRefreshControl()
-        self.registerCellWithIdentifier(identifier: String(describing: CountriesViewCell.self))
+        self.registerCellWithIdentifier(identifier: String(describing: CountriesPortraitViewCell.self))
+        self.registerCellWithIdentifier(identifier: String(describing: CountriesLandscapeViewCell.self))
         self.loadCounties()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        if UIDevice.current.orientation.isLandscape {
+            self.landscapeOrientation = true
+            self.tableView.reloadData()
+            print("Landscape")
+        } else {
+            self.landscapeOrientation = false
+            self.tableView.reloadData()
+            print("Portrait")
+        }
     }
     
     // MARK: - Private
@@ -47,7 +62,7 @@ class CountriesViewController: UIViewController, ViewControllerRootView, UITable
     }
     
     @objc private func refreshView() {
-        self.requestPage = defaultPage()
+//        self.requestPage = defaultPage()
         DatabaseController.deleteAll(entityType: Country.self)
         self.loadCounties()
         self.refreshControl?.endRefreshing()
@@ -58,18 +73,23 @@ class CountriesViewController: UIViewController, ViewControllerRootView, UITable
         self.tableView.reloadData()
     }
     
-    private func loadCounties(forPage: Int = 1, primaryLoad: Bool = true) {
-        if primaryLoad == true {
-            DatabaseController.deleteAll(entityType: Country.self)
-        }
-        
-        let stringPage = String(forPage)
-        NetworkModel.load(page: stringPage, block: addObjects)
+//    private func loadCounties(forPage: Int = 1, primaryLoad: Bool = true) {
+//        if primaryLoad == true {
+//            DatabaseController.deleteAll(entityType: Country.self)
+//        }
+//        
+//        let stringPage = String(forPage)
+//        NetworkModel.loadCountries(page: stringPage, block: addObjects)
+//    }
+    
+    private func loadCounties() {
+        DatabaseController.deleteAll(entityType: Country.self)
+        NetworkModel.loadCountries(block: addObjects)
     }
     
-    private func defaultPage() -> Int {
-        return 1
-    }
+//    private func defaultPage() -> Int {
+//        return 1
+//    }
     
     private func registerCellWithIdentifier(identifier: String) {
         self.tableView.register(UINib(nibName: identifier, bundle: nil),
@@ -83,11 +103,17 @@ class CountriesViewController: UIViewController, ViewControllerRootView, UITable
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: CountriesViewCell.self)) as! CountriesViewCell
-        
-        cell.fillWithObject(object: (self.countries?[indexPath.row])!)
-        return cell
-        
+        if self.landscapeOrientation == true {
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: CountriesLandscapeViewCell.self)) as! CountriesLandscapeViewCell
+            cell.fillWithObject(object: (self.countries?[indexPath.row])!)
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: CountriesPortraitViewCell.self)) as! CountriesPortraitViewCell
+            cell.fillWithObject(object: (self.countries?[indexPath.row])!)
+            
+            return cell
+        }
     }
     
     // MARK: - UITableViewDelegate
@@ -96,13 +122,13 @@ class CountriesViewController: UIViewController, ViewControllerRootView, UITable
         tableView.deselectRow(at: indexPath as IndexPath, animated: true)
     }
     
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        let section = tableView.numberOfSections - 1
-        let lastRow = tableView.numberOfRows(inSection: section) - 1
-        
-        if indexPath.row == lastRow {
-            self.requestPage += 1
-            self.loadCounties(forPage: self.requestPage, primaryLoad: false)
-        }
-    }
+//    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+//        let section = tableView.numberOfSections - 1
+//        let lastRow = tableView.numberOfRows(inSection: section) - 1
+//        
+//        if indexPath.row == lastRow {
+//            self.requestPage += 1
+//            self.loadCounties(forPage: self.requestPage, primaryLoad: false)
+//        }
+//    }
 }
